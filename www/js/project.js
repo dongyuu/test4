860cc51a9ca8e1fa7fe5c175361df0593e7b9780
dongyuu <17854225286@163.com>
2017-06-15 21:31:33 +0800

    Add files via upload

---
+++ www/js/project.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function btnClick()
+{
+	if(document.getElementById("name").value=="alice" && document.getElementById("pass").value=="123"){
+	alert("hello");}
+	else{
+	alert("wrong");}
+}
